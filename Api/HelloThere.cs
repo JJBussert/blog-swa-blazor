e7be8853a65d7e1b74a2c8dcadc5f8d16e8dbe3f
JJ Bussert <jj.bussert@gmail.com>
2023-03-31 08:56:01 -0500

    Initial Commit

---
+++ Api/HelloThere.cs
@@ -0,0 +1,38 @@
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public class HelloThere
+    {
+        private readonly ILogger<HelloThere> _logger;
+
+        public HelloThere(ILogger<HelloThere> log)
+        {
+            _logger = log;
+        }
+
+        [FunctionName("HelloThere")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new OkObjectResult("General Kenobi");
+        }
+    }
+}
+
